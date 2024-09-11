f8d46b742b5ab84cdd140025a2bb7366ae159e71
fernandes1414 <105095251+fernandes1414@users.noreply.github.com>
2024-09-11 10:05:48 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// var navbarLinks = document.querySelectorAll('.');
+
+
+// navbarLinks.forEach(function (link) {
+//     link.addEventListener('click', function (event) {
+
+//         var target = document.querySelector(link.getAttribute('href'));
+
+
+//         event.preventDefault();
+
+
+//         window.scrollTo({
+//             top: target.offsetTop,
+//             behavior: 'smooth'
+//         });
+//     });
+// });
